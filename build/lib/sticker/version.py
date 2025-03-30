15b86c75b147c6fdadee45b5c1811f8206f6dd7b
yumusubi <admin@yumusubi.dev>
2025-03-30 11:05:30 -0400

    add spongebob

--- build/lib/sticker/version.py
+++ build/lib/sticker/version.py
@@ -1,6 +1,6 @@
 # Generated in setup.py
 
 git_tag = None
-git_revision = 'unknown'
-version = '0.1.0+dev.unknown'
-linkified_version = '0.1.0+dev.unknown'
+git_revision = '839cdc2f'
+version = '0.1.0+dev.839cdc2f'
+linkified_version = '0.1.0+dev.[839cdc2f](https://github.com/maunium/stickerpicker/commit/839cdc2f5f81a8e3dd33c2b7317f377fd44fea24)'
